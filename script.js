c4533e880cba43be4c83a2f5ff90f73af9f21bf7
Anaina R <56787091+anaina1414@users.noreply.github.com>
2020-08-19 14:41:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function change()
+    {
+      document.getElementById("dashboard").style.display = "block";
+      document.getElementById("clss").style.display = "none";
+    }
+function change1()
+    {
+      document.getElementById("dashboard").style.display = "none";
+      document.getElementById("clss").style.display = "block";
+    }
